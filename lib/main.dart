7effa1c4e3ca6b9e8c58e406c215edeaa8682768
Ashish Shingade <66113291+Ashishhshingade@users.noreply.github.com>
2022-06-21 15:45:14 +0530

    main changes and product fetch done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:attoform/models/client.dart';
-<<<<<<< Updated upstream
-import 'package:attoform/screens/ClientM.dart';
-=======
 import 'package:attoform/models/product.dart';
->>>>>>> Stashed changes
 import 'package:attoform/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:attoform/screens/mainpage.dart';
